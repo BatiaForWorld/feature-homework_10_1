cd707b7a0ebeabc6cf257d7a7895ab518c41f8dc
Andrey Kazantsev <nwo2050@tutanota.com>
2024-08-05 21:30:17 +0300

    fix project

--- scr/mascs.py
+++ scr/mascs.py
@@ -1,14 +1,28 @@
-def mask_card_number(card_number: int) -> str:
-    """Преобразуем номер карты в строку"""
-    card_number_str = str(card_number)
-    """ Маскируем номер карты """
-    masked_card = card_number_str[:4] + " " + card_number_str[4:6] + "**" + " " + "****" + " " + card_number_str[-4:]
-    return masked_card
+"""Функцию маскировки номера банковской карты """
 
 
-def mask_account_number(account_number: int) -> str:
-    """Преобразуем номер счета в строку"""
-    account_number_str = str(account_number)
-    """ Маскируем номер счета """
-    masked_account = "**" + account_number_str[-4:]
-    return masked_account
+def get_masc_card_number(card_number: str) -> str:
+    """Функция принимает на вход
+    номер карты в виде числа и
+    возвращает маску номера по правилу XXXX XX** **** XXXX"""
+    if len(card_number) == 16 and card_number.isdigit():
+        card = str(card_number)
+        card_number = card[0:4] + " " + card[4:6] + "**" + " " + "****" + " " + card[12:16]
+        return card_number
+    else:
+        return "Введите 16 значный номер вашей карты"
+
+
+"""Функцию маскировки номера банковского счета"""
+
+
+def get_masc_account(account_number: str) -> str:
+    """Функция принимает на вход
+    номер счета  в виде числа и
+    возвращает маску номера по правилу  ** XXXX"""
+    if len(account_number) == 20 and account_number.isdigit():
+        number = str(account_number)
+        account_number = "**" + number[16:20]
+        return account_number
+    else:
+        return "Введите 20 значный номер вашего счёта"
--- scr/widget.py
+++ scr/widget.py
@@ -1,39 +1,27 @@
-from scr.mascs import mask_account_number, mask_card_number
+from scr.mascs import get_masc_account, get_masc_card_number
+
+"""функция, которая умеет обрабатывать информацию
+    как о картах, так и о счетах."""
 
 
-def mask_info(info: str) -> str:
-    """Маскирует номер карты или счета в зависимости от типа информации"""
-    if "Счет" in info:
-        account_number = int(info.split()[-1])
-        return "Счет " + mask_account_number(account_number)
+def masc_account_card(info: str) -> str:
+    info_masc = info.split()
+    if len(info_masc[-1]) == 20:
+        return "Счёт" + " " + get_masc_account(info_masc[-1])
+    elif len(info_masc[-1]) == 16:
+        return " ".join(info_masc[:-1]) + " " + get_masc_card_number(info_masc[-1])
     else:
-        card_number = int("".join(info.split()[2:]).replace(" ", ""))
-        return " ".join(info.split()[:2]) + " " + mask_card_number(card_number)
+        return "Введите корректные данные"
 
 
-""" Примеры использования функции """
-info1 = "Visa Platinum 7000 7922 8960 6361"
-info2 = "Счет 73654108430135874305"
-
-print(mask_info(info1))
-print(mask_info(info2))
-
-"""Функция, которая принимает на вход строку вида
- 2018-07-11T02:26:18.671407
- и возвращает строку с датой в виде
-11.07.2018"""
+"""Функция, которая принимает на вход строку с датой: 2024-03-11T02:26:18.671407
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
 
 
-def format_date(date_str: str) -> str:
+def get_date(date: str) -> str:
     """Разбиваем строку на части по символу 'T'"""
-    date_part = date_str.split("T")[0]
-    """Разбиваем дату на составные части"""
-    year, month, day = map(int, date_part.split("-"))
-    """Форматируем дату в нужный формат"""
-    formatted_date = f"{day:02d}.{month:02d}.{year}"
-    return formatted_date
-
-
-"""Пример использования функции"""
-date_input = "2018-07-11T02:26:18.671407"
-print(format_date(date_input))
+    date_only = date.split("T")[0]
+    year, month, day = map(int, date_only.split("-"))
+    """Объединяем дату на: число, месяц, год."""
+    formated_date = f"{day:02d}.{month:02d}.{year}"
+    return formated_date
